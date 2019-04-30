72689481129c2b2f376f3a3f6952468edbe86d41
Nate Boyette <nateboyette@icloud.com>
2019-04-29 18:35:16 -0700

    Refactored bubble sort.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -26,9 +26,7 @@ def bubble_sort(arr):
     for i in range(0, len(arr)):
         for j in range(i, len(arr)):
             if arr[i] > arr[j]:
-                temp = arr[j]
-                arr[j] = arr[i]
-                arr[i] = temp
+                arr[i], arr[j] = arr[j], arr[i]
             else:
                 pass
 
